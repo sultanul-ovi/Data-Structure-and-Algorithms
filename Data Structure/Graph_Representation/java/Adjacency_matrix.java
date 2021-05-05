5d2c3d1fdddba8ab3e47c1becfeb5d5154792a7d
Md. Sultanul Islam Ovi <sultanulislam@iut-dhaka.edu>
2021-05-05 22:29:21 +0600

    BST java

--- Data Structure/Graph_Representation/java/Adjacency_matrix.java
+++ Data Structure/Graph_Representation/java/Adjacency_matrix.java
@@ -1,32 +1,27 @@
-// Adjacency Matrix representation in Java
+// Java Implementation
+// Adjacency Matrix
 
-public class Graph {
+public class Adjacency_matrix {
   private boolean adjMatrix[][];
   private int numVertices;
 
-  // Initialize the matrix
-  public Graph(int numVertices) {
+  // Initialization
+  public Adjacency_matrix(int numVertices) {
     this.numVertices = numVertices;
     adjMatrix = new boolean[numVertices][numVertices];
   }
 
-  // Add edges
+  // Adding Edges
   public void addEdge(int i, int j) {
     adjMatrix[i][j] = true;
     adjMatrix[j][i] = true;
   }
 
-  // Remove edges
-  public void removeEdge(int i, int j) {
-    adjMatrix[i][j] = false;
-    adjMatrix[j][i] = false;
-  }
 
-  // Print the matrix
+  // Printing the matrix
   public String toString() {
     StringBuilder s = new StringBuilder();
     for (int i = 0; i < numVertices; i++) {
-      s.append(i + ": ");
       for (boolean j : adjMatrix[i]) {
         s.append((j ? 1 : 0) + " ");
       }
@@ -36,14 +31,19 @@ public class Graph {
   }
 
   public static void main(String args[]) {
-    Graph g = new Graph(4);
+    Adjacency_matrix g = new Adjacency_matrix(6);
 
-    g.addEdge(0, 1);
+    System.out.print("\n");
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
+    g.addEdge(0, 3);
+
+    g.addEdge(1, 3);
+    g.addEdge(2, 1);
+
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
 
     System.out.print(g.toString());
+    System.out.print("\n");
   }
 }
--- Data Structure/Graph_Representation/python/Adjacency_matrix.py
+++ Data Structure/Graph_Representation/python/Adjacency_matrix.py
@@ -31,10 +31,13 @@ class Graph(object):
 if __name__ == '__main__':
     
     graph = Graph(6)
+    
     graph.add_edge(0, 2)
     graph.add_edge(0, 3)
+    
     graph.add_edge(1, 3)
     graph.add_edge(2, 1)
+    
     graph.add_edge(3, 4)
     graph.add_edge(4, 5)
     
