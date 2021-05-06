c7d01fe161b8129d3c86fa6fcbad4361e9cc0e70
Md. Sultanul Islam Ovi <sultanulislam@iut-dhaka.edu>
2021-05-06 13:34:08 +0600

    bst cpp

--- Data Structure/Binary_Search_Tree/c++/delete_node.cpp
+++ Data Structure/Binary_Search_Tree/c++/delete_node.cpp
@@ -1,5 +1,5 @@
 // C++ Implementation
-// binary Search Tree Insert Operation
+// binary Search Tree Delete Operation
 
 //  Visual Representation of the binary search tree
 //                 10
@@ -10,6 +10,8 @@
 //     / \    / \      / \     / \       
 //    1   4  6   9   11   16 22   27  
 
+
+
 #include <iostream>
 using namespace std;
 
@@ -18,7 +20,7 @@ struct node {
   struct node *left, *right;
 };
 
-// NODE
+
 struct node *newNode(int item) {
   struct node *temp = (struct node *)malloc(sizeof(struct node));
   temp->key = item;
@@ -26,11 +28,11 @@ struct node *newNode(int item) {
   return temp;
 }
 
-// To print in inorder method
+// Inorder 
 void inorder(struct node *root) {
   if (root != NULL) {
     inorder(root->left);
-    cout << root->key << " -> ";
+    cout <<  " -> " << root->key;
     inorder(root->right);
   }
 }
@@ -45,7 +47,7 @@ struct node *insert(struct node *node, int key) {
   return node;
 }
 
-// Find the inorder successor
+// inorder successor
 struct node *minValueNode(struct node *node) {
   struct node *current = node;
   while (current && current->left != NULL)
@@ -56,13 +58,11 @@ struct node *minValueNode(struct node *node) {
 // Delete Operation
 struct node *deleteNode(struct node *root, int key) {
   if (root == NULL) return root;
-
   if (key < root->key)
     root->left = deleteNode(root->left, key);
   else if (key > root->key)
     root->right = deleteNode(root->right, key);
   else {
-
     if (root->left == NULL) {
       struct node *temp = root->right;
       free(root);
@@ -72,8 +72,6 @@ struct node *deleteNode(struct node *root, int key) {
       free(root);
       return temp;
     }
-
-
     struct node *temp = minValueNode(root->right);
     root->key = temp->key;
     root->right = deleteNode(root->right, temp->key);
@@ -84,21 +82,37 @@ struct node *deleteNode(struct node *root, int key) {
 
 int main() {
   struct node *root = NULL;
-  root = insert(root, 8);
-  root = insert(root, 3);
-  root = insert(root, 1);
-  root = insert(root, 6);
-  root = insert(root, 7);
   root = insert(root, 10);
-  root = insert(root, 14);
+  root = insert(root, 2);
+  root = insert(root, 1);
   root = insert(root, 4);
+  root = insert(root, 9);
+
+  root = insert(root, 7);
+  root = insert(root, 6);
+  root = insert(root, 5);
+
+  root = insert(root, 11);
+  root = insert(root, 14);
+  root = insert(root, 19);
+
+  root = insert(root, 17);
+
+  root = insert(root, 16);
+  root = insert(root, 27);
+  root = insert(root, 22);
 
 
+
+
+
+
+
+  cout << "Inorder traversal: ";
   inorder(root);
 
-  cout << "\nAfter deleting 10\n";
-  root = deleteNode(root, 10);
+  cout << "\nAfter deleting 11\n";
+  root = deleteNode(root, 11);
+  cout << "Inorder traversal: ";
   inorder(root);
-
-
 }
